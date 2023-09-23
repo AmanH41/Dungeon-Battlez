94ca9e9342b224488e98a9d934438e9cab95c84e
MohammedH41 <122569808+MohammedH41@users.noreply.github.com>
2023-09-23 12:01:14 -0600

    collision/hitbox added and camera effects added to create depth

--- level.py
+++ level.py
@@ -11,12 +11,13 @@ class Level:
 		self.display_surface = pygame.display.get_surface()
 
 		# sprite group setup
-		self.visible_sprites = pygame.sprite.Group()
+		self.visible_sprites = YSortCameraGroup()
 		self.obstacle_sprites = pygame.sprite.Group()
 
 		# sprite setup
 		self.create_map()
 
+	#this method creats all spirtes 
 	def create_map(self):
 		for row_index,row in enumerate(WORLD_MAP):
 			for col_index, col in enumerate(row):
@@ -29,6 +30,31 @@ class Level:
 
 	def run(self):
 		# update and draw the game
-		self.visible_sprites.draw(self.display_surface)
+		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
-		debug(self.player.direction)
+
+		#debug(self.player.direction)  #show player direction 
+
+#this class is for camera and drawing the sprites
+class YSortCameraGroup(pygame.sprite.Group):
+	def __init__(self):
+
+		#general setup
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		#offset for camera placement 
+		self.half_width = self.display_surface.get_size()[0] // 2 	
+		self.half_height = self.display_surface.get_size()[1] // 2 		
+	
+		self.offset = pygame.math.Vector2()
+
+	def custom_draw(self, player):		#apply the offset based on player pos so camera centers on player
+
+		#getting offset 
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height		
+
+		#for sprite in self.sprites():
+		for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image, offset_pos)		
--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('character1/16xwalking/tile000.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (64,64))
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0, -24)
 
         self.direction = pygame.math.Vector2()
 
@@ -33,37 +34,37 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
     
+
     def move(self, speed):
         #if vector is not != 0 
         if self.direction.magnitude() != 0:
             #normalize the speed of diagonal movements 
             self.direction = self.direction.normalize()
 
-        self.rect.x += self.direction.x * speed
+        self.hitbox.x += self.direction.x * speed
         self.collsion('horizontal')
-        self.rect.y += self.direction.y * speed
+        self.hitbox.y += self.direction.y * speed
         self.collsion('vertical')
- 
+        self.rect.center = self.hitbox.center
 
     def collsion(self,direction):
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
-                if sprite.rect.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.x > 0:
-                        self.rect.right = sprite.rect.left #if player is moving to the right 
+                        self.hitbox.right = sprite.hitbox.left #if player is moving to the right 
                     if self.direction.x < 0 :
-                        self.rect.left = sprite.rect.right # if palyer is moving to the left 
+                        self.hitbox.left = sprite.hitbox.right # if palyer is moving to the left 
 
         if direction == 'vertical':
             for sprite in self.obstacle_sprites:
-                if sprite.rect.colliderect(self.rect):
-                    if self.direction.y > 0: # moving down
-                        self.rect.bottom = sprite.rect.top
-                    if self.direction.y < 0: # moving up 
-                        self.rect.top = sprite.rect.bottom
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0: # moving down on a object 
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0: # moving up on a object
+                        self.hitbox.top = sprite.hitbox.bottom
 
-        
-
+    
 
     def update(self):
        self.input()
