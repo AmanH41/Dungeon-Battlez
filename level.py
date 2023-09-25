ee1b483dd409c2b4d3132a8f3b8e6e172bd23b6c
MohammedH41 <122569808+MohammedH41@users.noreply.github.com>
2023-09-25 01:27:20 -0600

    map CSV created

--- level.py
+++ level.py
@@ -3,6 +3,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from importCSV import *
+from random import choice
 
 class Level:
 	def __init__(self):
@@ -19,14 +21,35 @@ class Level:
 
 	#this method creats all spirtes 
 	def create_map(self):
-		for row_index,row in enumerate(WORLD_MAP):
-			for col_index, col in enumerate(row):
-				x = col_index * TILESIZE
-				y = row_index * TILESIZE
-				if col == 'x':
-					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
-				if col == 'p':
-					self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+		layouts = {
+			'boundary': import_csv_layout('Assets/DungeonCVS/Dungeon_Boundaryblocks.csv'),
+			'entities': import_csv_layout('Assets/DungeonCVS/Dungeon_entities.csv'),
+			'objects' : import_csv_layout('Assets/DungeonCVS/Dungeon_objects.csv')
+		}
+
+		graphics = {
+			'objects': import_folder('Assets\64xAssets\64xobjects')
+		}		
+
+		for style, layout in layouts.items():
+				for row_index,row in enumerate(layout):
+					for col_index, col in enumerate(row):
+						if col != '-1':
+							x = col_index * TILESIZE
+							y = row_index * TILESIZE
+							
+							if style == 'boundary':
+								Tile((x,y),[self.obstacle_sprites],'invisible')
+
+							if style == 'object':
+								surf = graphics['objects'][int(col)]
+								Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
+
+							if style == 'entities':
+								if col == '0':
+									self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+								else:
+									pass
 
 	def run(self):
 		# update and draw the game
@@ -39,21 +62,28 @@ class Level:
 class YSortCameraGroup(pygame.sprite.Group):
 	def __init__(self):
 
-		#general setup
 		super().__init__()
 		self.display_surface = pygame.display.get_surface()
 		#offset for camera placement 
 		self.half_width = self.display_surface.get_size()[0] // 2 	
 		self.half_height = self.display_surface.get_size()[1] // 2 		
-	
 		self.offset = pygame.math.Vector2()
 		
+		#creating the floor 
+		self.floor_surf = pygame.image.load('Tilemap/DungeonMap.png').convert()
+		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
+
 	#apply the offset based on player pos so camera centers on player
 	def custom_draw(self, player):		
 
-		#getting offset 
+		#getting offset from player
 		self.offset.x = player.rect.centerx - self.half_width
-		self.offset.y = player.rect.centery - self.half_height		
+		self.offset.y = player.rect.centery - self.half_height	
+
+		#drawing the floor 
+		floor_offset_pos = self.floor_rect.topleft - self.offset	
+		self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
 		#for sprite in self.sprites():
 		for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
--- tile.py
+++ tile.py
@@ -2,10 +2,9 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-	def __init__(self,pos,groups):
+	def __init__(self,pos,groups,sprite_type,surface = pygame.Surface((TILESIZE,TILESIZE))):
 		super().__init__(groups)
-		self.image = pygame.image.load('Assets/test/rock.png').convert_alpha()
-		self.image = pygame.transform.scale(self.image,(64,64))
-		
+		self.sprite_type = sprite_type
+		self.image = surface
 		self.rect = self.image.get_rect(topleft = pos)
-		self.hitbox = self.rect.inflate(-2, -10)
+		self.hitbox = self.rect.inflate(0,-10)
